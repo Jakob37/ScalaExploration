a3776ded9b0b7dd34a40ecab34be86bbe6f96426
Jakob Willforss <jakob.willforss@hotmail.com>
2016-12-28 10:05:59 +0100

    Lexical scoping implemented for Newton square example

--- ScalaCourse/newtons_square_roots.scala
+++ ScalaCourse/newtons_square_roots.scala
@@ -3,20 +3,27 @@
 // Set initial estimate
 // Repeatedly improve estimate by taking mean of y and x/y
 
+// Also, lexical scoping allowing function separation without namespace pollution
+// Furthermore, it allows reduction of explicit x-usage (not sure I agree fully here?)
+
 object session {
 
-    def sqrtIter(guess: Double, x: Double): Double =
-        if (isGoodEnough(guess, x)) guess
-        else sqrtIter(improve(guess, x), x)
+    def sqrt(x: Double) = {
+
+        def sqrtIter(guess: Double): Double =
+            if (isGoodEnough(guess)) guess
+            else sqrtIter(improve(guess))
 
 
-    def isGoodEnough(guess: Double, x: Double): Boolean =
-        Math.abs(guess * guess -x) / x < 0.001
+        def isGoodEnough(guess: Double): Boolean =
+            Math.abs(guess * guess - x) / x < 0.001
 
-    def improve(guess: Double, x: Double): Double =
-        (guess + x / guess) / 2
+        def improve(guess: Double): Double =
+            (guess + x / guess) / 2
 
-    def sqrt(x: Double): Double = sqrtIter(1.0, x)
+
+        sqrtIter(1.0)
+    }
 }
 
 println(session.sqrt(2))
