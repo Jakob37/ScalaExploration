34070d995409799ab81dcc84f8c6f9ea65e795a0
Jakob Willforss <jakob.willforss@hotmail.com>
2017-01-15 14:26:02 +0100

    Week 5 wrapup

---
+++ ScalaCourse/l56_reasoning_about_concat.scala
@@ -0,0 +1,12 @@
+// Functional programming - more available for reasoning?
+
+// What would it mean to claim that ++ is correct?
+// Can we prove its properties?
+
+// We can do natural induction - Prove base case, and all increment cases
+
+// Referential transparency
+
+// Structural induction
+
+
